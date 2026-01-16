68242cca9877e2e210900793f5059f9aa29b2682
Gaurav Jha <gauravgha316@gmail.com>
2026-01-16 22:42:26 +0530

    Update existing file

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 // const heading=React.createElement("h1", {id:"heading"},("hello world from react"))
 // const root=ReactDOM.createRoot(document.getElementById("root"))
 // root.render(heading)
 
 
 const parent=React.createElement("div", {id:"Parents"},React.createElement("div",{id:"child"},
-[React.createElement("h1", {}, "I am an h1 tag"),React.createElement("h2", {}, "I am an h2 tag")] ),
+[React.createElement("h1", {}, "I am an h1 tag"),React.createElement("h2", {}, "I am an h2 tag💞")] ),
 
 React.createElement("div",{id:"child2"},
 [React.createElement("h1", {}, "I am an h1 tag"),React.createElement("h2", {}, "I am an h2 tag")] )
 )
 console.log(parent)
 
-const root=ReactDOM.createRoot(document.getElementById("root"))
+const root=createRoot(document.getElementById("root"))
 
 
 root.render(parent);
