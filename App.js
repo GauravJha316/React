a9acec17ccf278988f06101a8239faa0c9b40fb1
Gaurav Jha <gauravgha316@gmail.com>
2026-01-20 23:37:01 +0530

    your message

--- App.js
+++ App.js
@@ -2,33 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
 
-//react element using react
-// const heading=React.createElement("h1",{id:"heading"},"namaste React")
-// console.log(heading)
-
-//react element using jsx
-
-
-// React element
-// const elem=<span>React element</span>;
-// const Title = ()=> (
-
-//     <h1 className="head">Namaste React using JSX</h1>
-// );
-
-// //ReactComponent
-// //its a Component composition 
-// const HeadingComponent =() =>(
-//     <div id="container">
-        
-//         <h1 className="heading"> Namaste React</h1>
-//         <Title/>
-//     </div> 
-// )
-
-// const root=ReactDOM.createRoot(document.getElementById("root"));
- 
-// // root.render(heading);
 const Header=()=>{
     return(
         <div className="header">
@@ -48,26 +21,95 @@ const Header=()=>{
 }
 
 
-const RestaurantCard=()=>{
+const RestaurantCard=(props)=>{
+    const {resData}=props;
     return(
         <div className="res-card">
             <img alt="res-logo" className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/FOOD_CATALOG/IMAGES/CMS/2025/8/21/a2d65774-7a0e-4d82-b48e-f7ea48e9952c_9df7225b-ceae-4f5f-bdc2-a6e05e0d8371.jpeg"/>
-            <h3>Vanilla Bento Cake
-                </h3>
-            <h4>Briyani North India, Aisan</h4> 
-            <h3>4.4 star</h3>   
-            <h3>38 minute</h3>
+            <h3>{resData.resName}</h3>
+            {/* <h4>{resData.cuisine.join(",")}</h4>  */}
+            <h3>{resData.avgRating}</h3>   
+            <h3>{resData.costForTwo}</h3>
         </div>
     )
 }
+
+const resObj = {
+  statusCode: 0,
+  data: {
+    cards: [
+      {
+        card: {
+          card: {
+            "@type": "type.googleapis.com/swiggy.gandalf.widgets.v2.GridWidget",
+            header: { title: "Restaurants near you" },
+            gridElements: {
+              infoWithStyle: {
+                restaurants: [
+                  {
+                    info: {
+                      id: "1001",
+                      name: "Burger Hub",
+                      cloudinaryImageId: "dummy_burger_01",
+                      locality: "Indiranagar",
+                      areaName: "Bengaluru",
+                      cuisines: ["Burgers", "Fast Food"],
+                      avgRating: 4.3,
+                      totalRatingsString: "10K+ ratings",
+                      sla: { deliveryTime: 24, slaString: "24 mins" },
+                      costForTwo: "₹250 for two"
+                    }
+                  },
+                  {
+                    info: {
+                      id: "1002",
+                      name: "Spice Bowl",
+                      cloudinaryImageId: "dummy_indian_02",
+                      locality: "Koramangala",
+                      areaName: "Bengaluru",
+                      cuisines: ["North Indian", "Biryani"],
+                      avgRating: 4.5,
+                      totalRatingsString: "25K+ ratings",
+                      sla: { deliveryTime: 30, slaString: "30 mins" },
+                      costForTwo: "₹400 for two"
+                    }
+                  },
+                  {
+                    info: {
+                      id: "1003",
+                      name: "Pizza Point",
+                      cloudinaryImageId: "dummy_pizza_03",
+                      locality: "HSR Layout",
+                      areaName: "Bengaluru",
+                      cuisines: ["Pizzas", "Italian"],
+                      avgRating: 4.1,
+                      totalRatingsString: "8K+ ratings",
+                      sla: { deliveryTime: 28, slaString: "28 mins" },
+                      costForTwo: "₹500 for two"
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      }
+    ]
+  }
+};
+
 const Body =()=>{
+    const firstRestaurant =
+    resObj?.data?.cards?.[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.[0]?.info;   
     return(
+
         <div className="body">
             <div className="search">
                 Search
             </div>
             <div className="res-container">
-                <RestaurantCard/>  <RestaurantCard/>  <RestaurantCard/>  <RestaurantCard/>  <RestaurantCard/>  <RestaurantCard/>  <RestaurantCard/>
+                <RestaurantCard resData={firstRestaurant[0]}/>  
+                              <RestaurantCard resData={firstRestaurant[1]}/>  
             </div>
         </div>
     )
